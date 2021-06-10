008d1405e091bb7c169e82770cfd68f4c671da6f
zhaowq0807 <zhaowq2021@126.com>
2021-06-10 15:11:18 +0800

    update member main

--- shop-member-api/src/main/java/com/fh/shop/api/ShopMemberApp.java
+++ shop-member-api/src/main/java/com/fh/shop/api/ShopMemberApp.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ShopMemberApp {
 
     public static void main(String[] args) {
+        System.out.println("今天学习了Git");
         SpringApplication.run(ShopMemberApp.class,args);
     }
 }
